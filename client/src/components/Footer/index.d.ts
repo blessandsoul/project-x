2ddc839e9ced72c569ac7bacf10ba3d4453c34a5
Bless&Soul <blessandsoul@trendingnow.ge>
2025-11-27 00:39:23 +0400

    push

---
+++ client/src/components/Footer/index.d.ts
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface FooterLink {
+  id: string;
+  label: string;
+  href: string;
+}
+
+interface FooterProps {
+  footerLinks: FooterLink[];
+  onNavigate?: (href: string) => void;
+}
+
+declare const Footer: FC<FooterProps>;
+export default Footer;
